a72b47bb3eedc9956804b1f867ddce7bab37fa81
juancho <jm.ticona.p@gmail.com>
2022-08-15 22:34:04 -0500

    [FIX] efecto modal en footer

--- assets/js/components/cartMenu.js
+++ assets/js/components/cartMenu.js
@@ -4,18 +4,21 @@ export function cartMenu() {
   const cartClose = document.getElementById('cart-close')
   const main = document.querySelector("main.main")
   const nav = document.querySelector("nav")
+  const footer = document.querySelector('footer')
 
   if (cartToggle) {
     cartToggle.addEventListener('click', (evt) => {
-      nav.classList.toggle("modal-efect")
-      main.classList.toggle("modal-efect")
+      nav.classList.toggle("modal-effect")
+      main.classList.toggle("modal-effect")
+      footer.classList.toggle("modal-effect")
       cart.classList.toggle('show-cart')
     })
   }
 
   const closeCart = () => {
-    nav.classList.remove("modal-efect")
-    main.classList.remove("modal-efect")
+    nav.classList.remove("modal-effect")
+    main.classList.remove("modal-effect")
+    footer.classList.remove("modal-effect")
     cart.classList.remove('show-cart')
   }
 
